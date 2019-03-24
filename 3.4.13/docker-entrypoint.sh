eaffb4651a8dc183bcf3a605733fb79889314eef
Raghavendra D Prabhu <me@rdprabhu.com>
2019-03-24 17:43:46 +0000

    Change entrypoint to make it work with k8s operator

--- 3.4.13/docker-entrypoint.sh
+++ 3.4.13/docker-entrypoint.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
+# This entrypoint works only with kubernetes, not meant to be run directly.
 
-set -e
+set -eux
 
-# Allow the container to be started with `--user`
 if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
     chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
     exec su-exec "$ZOO_USER" "$0" "$@"
 fi
 
-# Generate the config only if it doesn't exist
-if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
-    CONFIG="$ZOO_CONF_DIR/zoo.cfg"
+CONFIG="$ZOO_CONF_DIR/zoo.cfg"
 
-    echo "clientPort=$ZOO_PORT" >> "$CONFIG"
-    echo "dataDir=$ZOO_DATA_DIR" >> "$CONFIG"
-    echo "dataLogDir=$ZOO_DATA_LOG_DIR" >> "$CONFIG"
+echo "clientPort=$ZOO_PORT" >> "$CONFIG"
+echo "dataDir=$ZOO_DATA_DIR" >> "$CONFIG"
+echo "dataLogDir=$ZOO_DATA_LOG_DIR" >> "$CONFIG"
 
-    echo "tickTime=$ZOO_TICK_TIME" >> "$CONFIG"
-    echo "initLimit=$ZOO_INIT_LIMIT" >> "$CONFIG"
-    echo "syncLimit=$ZOO_SYNC_LIMIT" >> "$CONFIG"
+echo "tickTime=$ZOO_TICK_TIME" >> "$CONFIG"
+echo "initLimit=$ZOO_INIT_LIMIT" >> "$CONFIG"
+echo "syncLimit=$ZOO_SYNC_LIMIT" >> "$CONFIG"
 
-    echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT" >> "$CONFIG"
-    echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL" >> "$CONFIG"
-    echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS" >> "$CONFIG"
+echo "autopurge.snapRetainCount=$ZOO_AUTOPURGE_SNAPRETAINCOUNT" >> "$CONFIG"
+echo "autopurge.purgeInterval=$ZOO_AUTOPURGE_PURGEINTERVAL" >> "$CONFIG"
+echo "maxClientCnxns=$ZOO_MAX_CLIENT_CNXNS" >> "$CONFIG"
 
-    for server in $ZOO_SERVERS; do
-        echo "$server" >> "$CONFIG"
-    done
-fi
+myhostname=$(hostname)
 
-# Write myid only if it doesn't exist
-if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
-    echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
-fi
+myord=$(cut -d- -f2 <<< $myhostname)
+htemp=$(cut -d- -f1 <<< $myhostname)
+sdomain=$(hostname -f | cut -d. -f2)
+myord=$((myord+1))
+
+
+for id in ${ZOO_IDS//,/ }; do
+    tid=$(( id-1 ))
+    echo "server.${id}=${htemp}-${tid}.${sdomain}:2888:3888" >> "$CONFIG"
+done
+
+echo $myord > "$ZOO_DATA_DIR/myid"
 
 exec "$@"
